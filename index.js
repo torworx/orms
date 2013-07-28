ea62419b826b2206f8e895a560d5ec4849f1e7ce
taoyuan <taoyuan@oyo.im>
2013-07-28 14:57:10 +0800

    fixed load params error

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ exports.loader = function(orm) {
     return new Loader(orm);
 };
 
-exports.load = function(orm, schema, location) {
-    return exports.loader(orm).load(schema, location);
+exports.load = function(orm) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    var loader = new Loader(orm);
+    return loader.load.apply(loader, args);
 };
 
 exports.Loader = Loader;
